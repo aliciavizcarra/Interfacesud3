7f554af2cfea3b1ee4580cd51c69ce48e905efc0
IsisTD <220126@fppiramide.com>
2023-11-28 19:42:30 +0100

    update

---
+++ ej8.js
@@ -0,0 +1,5 @@
+const showButton = document.querySelector("#show-button");
+showButton.addEventListener("click", function () {
+  const alertDialog = document.querySelector("#alert-dialog");
+  alertDialog.showModal();
+});
